f64cad2e8f48611f2f847970757fe5b1e85544d2
Julian <jfisher009@drury.edu>
2020-09-29 11:02:20 -0500

    Test Commit

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -1,2 +1,16 @@
 public class SudokuSolver {
+
+    public SudokuSolver(){
+    }
+
+    public int[][] solve(int[][] givenPuzzle){
+        if(!(givenPuzzle.length == 9  && givenPuzzle[0].length == 9)){
+            throw new IllegalArgumentException("Input should be a 9X9 array");
+;        }
+
+    }
+
+    private int[][] generatePossibleValuesGrid(){
+
+    }
 }
